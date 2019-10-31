e0e6927884abc393a16df59b9fd48029bbc421a8
Handriki <handriki.kasa@student.umn.ac.id>
2019-10-31 11:47:49 +0700

    adding posting interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'message', loadChildren: './message/message.module#MessagePageModule' },
-  { path: 'message/open-message', loadChildren: './message/open-message/open-message.module#OpenMessagePageModule' },  { path: 'user-profile', loadChildren: './user-profile/user-profile.module#UserProfilePageModule' },
-
-
-
+  { path: 'message/open-message', loadChildren: './message/open-message/open-message.module#OpenMessagePageModule' },
+  { path: 'user-profile', loadChildren: './user-profile/user-profile.module#UserProfilePageModule' },
 ];
 
 @NgModule({
